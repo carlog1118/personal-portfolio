c22cf12968c7b306b78593de14565cc48fcb35d9
Carlo Giustozzi <carlog1118@gmail.com>
2019-11-14 19:30:47 -0500

    css and java

---
+++ index.js
@@ -0,0 +1,39 @@
+//open/close hamburger menu
+function toggleList(){
+    $('.hamburger').click(function(){
+        $('.nav-list').toggleClass('hidden');
+    });
+}
+
+function aboutScroll(){
+    $('#js-about-me').click(function(){
+        $('html, body').animate({
+            scrollTop: ($('.aboutme').offset().top-100)
+        },500)
+    });
+}
+
+function workScroll(){
+    $('#js-work').click(function(){
+        $('html, body').animate({
+            scrollTop: ($('.mywork').offset().top-100)
+        },500)
+    });
+}
+
+function contactScroll(){
+    $('#js-contact').click(function(){
+        $('html, body').animate({
+            scrollTop: ($('.contactme').offset().top-100)
+        },500)
+    });
+}
+    
+function domReady(){
+    toggleList();
+    aboutScroll();
+    workScroll();
+    contactScroll()
+}
+
+$(domReady);
